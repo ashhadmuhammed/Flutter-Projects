76a413b57772cad10f079f782efad8655145baea
Ashhad Muhammed Kader <93571194+ashhadmuhammed@users.noreply.github.com>
2023-12-29 15:01:43 +0530

    Add files via upload

---
+++ lib/Models/ExpDetails.dart
@@ -0,0 +1,11 @@
+// import 'dart:ffi';
+
+// class ExpDetails{
+//   final String? id;
+//   final DateTime  date;
+//   final String title;
+//   final String desciption;
+//   final Float amount;
+  
+
+// }
---
+++ lib/NavPage.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'DashboardPage.dart';
+import 'HomePage.dart';
+import 'MyexpensesPage.dart';
+
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // int _counter = 0;
+  int _selectedIndex = 0;
+  final List<Widget> _pages = [
+    HomePage(),
+    DashboardPage(),
+    MyexpensesPage(),
+  ];
+
+  // void _incrementCounter() {
+  //   setState(() {
+  //     _counter++;
+  //   });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: _pages[_selectedIndex],
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ),
+      
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Expenses',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
